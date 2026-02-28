c64b865697b06e8e529f8f8d1dda50163914e90b
Santiago <luquevillanuevasantiago@gmail.com>
2026-02-28 16:57:35 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.util.HashMap;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        HashMap<String, String> diccionario = new HashMap<>();
+
+        diccionario.put("España", "Madrid");
+        diccionario.put("Portugal", "Lisboa");
+        diccionario.put("Francia", "París");
+
+        while (true) {
+            String paisUsuario = System.console().readLine("Escribe el nombre de un país y te diré su capital: ");
+
+            if (paisUsuario.equals("salir")) {
+                break;
+            }
+
+            if (diccionario.containsKey(paisUsuario)) {
+                String capitalUsuario = diccionario.get(paisUsuario);
+                System.out.println("La capital de "+paisUsuario+" es "+capitalUsuario);
+            } else {
+                String nuevaCapital = System.console().readLine("No conozco esa capital, cuál es la capital de "+paisUsuario+": ");
+                diccionario.put(paisUsuario, nuevaCapital);
+                System.out.println("Gracias por enseñarme nuevas capitales.");
+            }
+        }
     }
 }
